0a5e3bbaf43924677a5fca14ae341df9f4234443
anubhavj02 <jainan@tcd.ie>
2017-12-06 22:44:41 +0000

    Commit 2: Cloning Github Repository in the server local

--- RepoComplexityCalculator.py
+++ RepoComplexityCalculator.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from git import Repo
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/CalculateComplexity')
+@app.route('/calculateComplexity')
 def calc_complexity():
-    return 'Calculating Complexity'
+    url = request.args.get('url')
+    name = (url.split('/')[-1]).split('.')[0]
+    print name
+    path = "/repos/" + name
+    Repo.clone_from(url, path)
+    return "success"
 
 
 if __name__ == '__main__':
