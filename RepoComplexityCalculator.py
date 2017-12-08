6f684ad44eead262161af7a09f898ca554b9bf70
anubhavj02 <jainan@tcd.ie>
2017-12-08 15:01:38 +0000

    Commit 5: Complexity Calculation of the python files in the repository

--- RepoComplexityCalculator.py
+++ RepoComplexityCalculator.py
@@ -3,6 +3,7 @@ from git import Repo
 from flask import request
 import shutil
 import os
+import subprocess
 
 app = Flask(__name__)
 
@@ -11,6 +12,16 @@ def create_file_list(file_list, dir, filenames):
     for filename in filenames:
         if filename.endswith('.py'):
             file_list.append(str(os.path.join(dir, filename).encode('ascii', 'ignore')))
+
+
+def complexity_analyzer(path):
+    raw_complexity = subprocess.Popen(["radon raw " + path + " -s -j"], stdout=subprocess.PIPE, shell=True,
+                                      executable='/bin/bash')
+    cyclomatic_complexity = subprocess.Popen(["radon cc " + path + " -s -j"], stdout=subprocess.PIPE, shell=True,
+                                             executable='/bin/bash')
+    (raw_complexity_output, err) = raw_complexity.communicate()
+    (cyclomatic_complexity, err1) = cyclomatic_complexity.communicate()
+    return raw_complexity_output + cyclomatic_complexity
 
 
 @app.route('/calculateComplexity')
@@ -26,6 +37,10 @@ def calc_complexity():
     path = os.path.expanduser(path)
     if os.path.isdir(path):
         os.path.walk(path, create_file_list, file_list)
+
+    for path in file_list:
+        print complexity_analyzer(path)
+
     return "success"
 
 
