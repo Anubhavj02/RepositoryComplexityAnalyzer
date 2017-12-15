80b35a73e381cd6c8f3d9cbec8964e6c1e40a955
anubhavj02 <jainan@tcd.ie>
2017-12-15 17:48:46 +0000

    Commit 17: Implementing the assign work to worker and get back results functionality

--- repo_complexity_analyzer_master_slave.py
+++ repo_complexity_analyzer_master_slave.py
@@ -3,6 +3,10 @@ import argparse
 from flask import Flask  # To implement rest service
 import time
 import repo_complexity_analyzer
+from flask import request
+from flask import jsonify
+import os
+import shutil
 
 app = Flask(__name__)
 
@@ -26,6 +30,28 @@ class ServerManagerNode:
         self.total_file = len(self.file_list)
 
 
+@app.route('/complexityFromSlave', methods=['GET'])
+def get_work():
+    """function to assign the work to the slaves
+    """
+
+    # If the current number slaves are less than the required
+    if server_manager_node.current_slave_count < server_manager_node.slave_count:
+        time.sleep(0.1)
+        return jsonify({'status': 'Waiting'})
+    # If the repo is empty
+    if len(server_manager_node.file_list) == 0:
+        return jsonify({'status': 'Done'})
+
+    # Pull the first file name
+    file_path = server_manager_node.file_list[0]
+    # Delete the first index such that the pointer points to the next
+    del server_manager_node.file_list[0]
+    print "Sending work to slave:"+file_path
+    # Sending the work as response to the slave with the file path to be analyzed
+    return jsonify({'status': file_path})
+
+
 @app.route('/registerSlave', methods=['GET'])
 def register_slave():
     """function to regsister slave to the master=
@@ -37,6 +63,41 @@ def register_slave():
     if server_manager_node.current_slave_count == server_manager_node.slave_count:
         server_manager_node.start_time = time.time()
     return "registered"
+
+
+def calculate_average_file_complexity():
+    # Check whether all files have been analyzed or not
+    if len(server_manager_node.file_complexity_list) == server_manager_node.total_file:
+        # Stop the timer
+        time_end = time.time() - server_manager_node.start_time
+        print("Execution Time taken: ", time_end)
+        print("No. of files analyzed:" + str(len(server_manager_node.file_complexity_list)))
+        # Calculating average complexity
+        average_complexity = 0
+        for file_comp in server_manager_node.file_complexity_list:
+            if file_comp['complexity'] > 0:
+                average_complexity += file_comp['complexity']
+            else:
+                print("No files to be analyzed")
+        average_complexity = average_complexity / len(server_manager_node.file_complexity_list)
+        print("Average Complexity of the repo: ", average_complexity)
+
+        # Remove the git cloned folder after use
+        if os.path.isdir(server_manager_node.path):
+            shutil.rmtree(server_manager_node.path)
+
+
+@app.route('/complexityFromSlave', methods=['POST'])
+def complexity_from_slave():
+    """function to receive the complexity from the slaves
+    """
+    code_complexity_req = request.json
+    file_path= code_complexity_req['file_path']
+    code_complexity= code_complexity_req['complexity']
+    # Add the file path and its complexity to the list
+    server_manager_node.file_complexity_list.append({'file_path': file_path, 'complexity': code_complexity})
+    calculate_average_file_complexity()
+    return jsonify({'success': True})
 
 
 if __name__ == '__main__':
